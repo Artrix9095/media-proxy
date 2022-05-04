fd5580da49bfd33a7af9a83760f7917a185755bd
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-05-03 20:00:42 -0700

    Fix invalid syntax

--- src/util/http.ts
+++ src/util/http.ts
@@ -1,5 +1,6 @@
-import { IncomingMessage } from "http";
+import { IncomingMessage } from 'http';
 
 export function getProtocol(req: IncomingMessage): 'http' | 'https' {
     // @ts-ignore
-    return (req.headers['x-forwarded-proto'] || req.connection.encrypted) ? 'https' : 'http';
+    return req.headers['x-forwarded-proto'] || req.connection.encrypted ? 'https' : 'http';
+}
